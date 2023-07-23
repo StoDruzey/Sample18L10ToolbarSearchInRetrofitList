4b1e5dd2367d4d39679f6b42c94d567579211d79
StoDruzey <sergelabada@gmail.com>
2023-07-23 23:36:47 +0200

    ready to retrofit

--- app/src/main/java/com/example/sample18l10toolbarsearchinretrofitlist/FirstFragment.kt
+++ app/src/main/java/com/example/sample18l10toolbarsearchinretrofitlist/FirstFragment.kt
@@ -1,6 +1,34 @@
 package com.example.sample18l10toolbarsearchinretrofitlist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.sample18l10toolbarsearchinretrofitlist.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentFirstBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
 }
