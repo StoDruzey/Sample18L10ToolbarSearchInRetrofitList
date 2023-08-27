c5823cfbec1d681ac995d1fd948d5b5443f715d3
StoDruzey <sergelabada@gmail.com>
2023-08-27 23:10:30 +0200

    Search in list completed

--- app/src/main/java/com/example/sample18l10toolbarsearchinretrofitlist/FirstFragment.kt
+++ app/src/main/java/com/example/sample18l10toolbarsearchinretrofitlist/FirstFragment.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.sample18l10toolbarsearchinretrofitlist.databinding.FragmentFirstBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import com.example.sample18l10toolbarsearchinretrofitlist.UserAdapter
 
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private var currentRequest: Call<List<User>>? = null
+    private val currentUsers = mutableListOf<User>()
+    private val adapter by lazy { UserAdapter(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,11 +37,66 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            recyclerView.adapter = adapter
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val githubInterface = retrofit.create<GithubInterface>()
+            currentRequest = githubInterface
+                .getUsers(30, 70)
+                .apply {
+                    enqueue(object : Callback<List<User>> {
+                        override fun onResponse(
+                            call: Call<List<User>>,
+                            response: Response<List<User>>
+                        ) {
+                            if (response.isSuccessful) {
+                                val users = response.body() ?: return
+                                currentUsers.addAll(users)
+                                adapter.submitList(users)
+                            } else {
+                                handleException(HttpException(response))
+                            }
+                        }
+
+                        override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                            if (!call.isCanceled) {
+                                handleException(t)
+                            }
+                        }
+                    })
+                }
+
+            toolbar
+                .menu
+                .findItem(R.id.menu_search)
+                .actionView
+                .let { it as SearchView }
+                .setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String): Boolean {
+                        return false
+                    }
+
+                    override fun onQueryTextChange(query: String): Boolean {
+                        adapter.submitList(currentUsers.filter { it.login.contains(query) })
+                        return true
+                    }
+                })
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
 
+        currentRequest?.cancel()
         _binding = null
+    }
+
+    private fun handleException(e: Throwable) {
+        Toast.makeText(requireContext(), e.message ?: "Something went wrong", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/sample18l10toolbarsearchinretrofitlist/UserAdapter.kt
+++ app/src/main/java/com/example/sample18l10toolbarsearchinretrofitlist/UserAdapter.kt
@@ -1,6 +1,50 @@
 package com.example.sample18l10toolbarsearchinretrofitlist
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.sample18l10toolbarsearchinretrofitlist.databinding.ItemUserBinding
+import com.example.sample18l10toolbarsearchinretrofitlist.UserViewHolder
 
-class UserAdapter : ListAdapter<> {
+class UserAdapter(
+    context: Context
+) : ListAdapter<User, UserViewHolder>(DIFF_UTIL) {
+
+    private val layoutInflater = LayoutInflater.from(context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(
+            binding = ItemUserBinding.inflate(layoutInflater, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.login == newItem.login && oldItem.avatarUrl == newItem.avatarUrl
+            }
+        }
+    }
+}
+
+class UserViewHolder(
+    private val binding: ItemUserBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: User) {
+        with(binding) {
+            imageAvatar.load(item.avatarUrl)
+            userName.text =item.login
+        }
+    }
 }
